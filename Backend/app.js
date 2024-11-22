60da4bc16dbdf20e18be54c0cd7374e76879dbdb
Saad Shahid <saadshahid.nls@gmail.com>
2024-11-22 13:00:19 +0500

    Added postman collection + routes

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var profileRouter = require('./routes/profile');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.use('/cover-letter', require('./cover-letter'));
+router.use('/faq', require('./faq'));
+router.use('/profile', require('./profile'));
+
 module.exports = router;
